3e629451edbd617486d88fb5234683301831661a
Amulya-naidu <naiduamulya1999@gmail.com>
2024-10-26 16:26:13 +0530

    changes

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "latest" {     # aws_ami helps to get AMI ID of the os
 resource "aws_instance" "jenkins-server" {   # we are creating a new instance for jenkins-server
     ami = data.aws_ami.latest.id    # we are using the latest ami that we fetched earlier
     instance_type = "t3.medium"     # This is the type of the instance we are creating
-    subnet_id = aws_subnet.our-public-subnet.id   # this is the id of the subnet we are using to launch the instance
+    # subnet_id = aws_subnet.our-public-subnet.id   # this is the id of the subnet we are using to launch the instance
     user_data = file("./jenkins-server.sh")  # this is the script that will be executed during the creation of the instance
     key_name = "mern-stack" # this is the key name that we have created in console
     root_block_device {
