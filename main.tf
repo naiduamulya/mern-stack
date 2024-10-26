75ed5647dd35d8d04ba592f97bb75256e1cb60b4
naiduamulya <naiduamulya29@gmail.com>
2024-10-26 10:38:23 +0000

    test

--- main.tf
+++ main.tf
@@ -2,43 +2,32 @@ provider "aws" {
   region = "us-east-1" # replace with your region
 }
 
-resource "aws_key_pair" "deploy_key" {
-  key_name   = "deploy_key"
-  public_key = file("~/.ssh/your_public_key.pub") # replace with path to your public key
+data "aws_ami" "latest" {     # aws_ami helps to get AMI ID of the os
+    most_recent = true  # this is the filter for most recent Ami
+   
+    filter {   # this is the filter for virtualization type
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+    filter {  # this is the filter for AMI name of the OS which can be found in AMI section in EC2
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+     owners = ["099720109477"]  # this is the owner of the OS, which can be found in AMI section in EC2
 }
 
-resource "aws_instance" "mern_instance" {
-  ami                    = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (or a compatible one)
-  instance_type          = "t2.micro"              # use appropriate instance type
-  key_name               = aws_key_pair.deploy_key.key_name
-  associate_public_ip_address = true
-  user_data = <<-EOF
-    #!/bin/bash
-    # Update packages
-    sudo yum update -y
-    # Install Node.js
-    curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-    sudo yum install -y nodejs
-    # Install MongoDB
-    sudo tee /etc/yum.repos.d/mongodb-org-4.4.repo <<EOF2
-    [mongodb-org-4.4]
-    name=MongoDB Repository
-    baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.4/x86_64/
-    gpgcheck=1
-    enabled=1
-    gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
-    EOF2
-    sudo yum install -y mongodb-org
-    sudo systemctl start mongod
-    sudo systemctl enable mongod
-    # Install PM2
-    sudo npm install -g pm2
-    # Create app directory
-    sudo mkdir -p /var/www/html
-    sudo chown ec2-user:ec2-user /var/www/html
-  EOF
 
-  tags = {
-    Name = "mern-server"
-  }
+resource "aws_instance" "jenkins-server" {   # we are creating a new instance for jenkins-server
+    ami = data.aws_ami.latest.id    # we are using the latest ami that we fetched earlier
+    instance_type = "t3.medium"     # This is the type of the instance we are creating
+    subnet_id = aws_subnet.our-public-subnet.id   # this is the id of the subnet we are using to launch the instance
+    user_data = file("./jenkins-server.sh")  # this is the script that will be executed during the creation of the instance
+    key_name = "mern-stack" # this is the key name that we have created in console
+    root_block_device {
+      volume_size = 20
+    }
+
+    tags = {
+        Name = "jenkins-server"  # this will provide name to instance 
+    }
 }
